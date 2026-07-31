0ed87ed2982b9c70c8baa038a3529a0cd4c018d0
Poserinka <111547033+Poserinka@users.noreply.github.com>
2026-07-31 05:43:16 +0200

    Update class-activator.php
    Add plugin activator class v0.1-alpha.1

--- includes/class-activator.php
+++ includes/class-activator.php
@@ -1,1 +1,52 @@
+<?php
 
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+
+class Dizzy_Activator {
+
+
+    /**
+     * Plugin activation
+     */
+    public static function activate() {
+
+
+        // Register post types before flushing rules
+        self::register_post_types();
+
+
+        // Refresh rewrite rules
+        flush_rewrite_rules();
+
+
+    }
+
+
+
+    /**
+     * Register CPTs during activation
+     */
+    private static function register_post_types() {
+
+
+        if ( class_exists( 'Dizzy_Events_Post_Type' ) ) {
+
+            $events = new Dizzy_Events_Post_Type();
+
+        }
+
+
+        if ( class_exists( 'Dizzy_Artists_Post_Type' ) ) {
+
+            $artists = new Dizzy_Artists_Post_Type();
+
+        }
+
+
+    }
+
+
+}
