be1c020dfe44ee74d4c0b69e93e86dc90a8fcdce
Poserinka <111547033+Poserinka@users.noreply.github.com>
2026-07-31 05:42:10 +0200

    Update class-loader.php
    Improve class loader structure v0.1-alpha.1

--- includes/class-loader.php
+++ includes/class-loader.php
@@ -1,1 +1,50 @@
+<?php
 
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+class Dizzy_Loader {
+
+
+    /**
+     * Load required classes
+     */
+    public static function init() {
+
+
+        $classes = array(
+
+            'class-activator.php',
+
+            'class-deactivator.php',
+
+            'class-plugin.php',
+
+            'class-events-post-type.php',
+
+            'class-artists-post-type.php',
+
+            'class-admin-menu.php',
+
+        );
+
+
+        foreach ( $classes as $class ) {
+
+
+            $file = DIZZY_EVENTS_PATH . 'includes/' . $class;
+
+
+            if ( file_exists( $file ) ) {
+
+                require_once $file;
+
+            }
+
+        }
+
+
+    }
+
+}
