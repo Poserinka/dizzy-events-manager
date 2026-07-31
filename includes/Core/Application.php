46ad879a85f64c1a8a24061b85b33f9a24e402a7
Poserinka <info@poserinka.com>
2026-08-01 01:58:46 +0200

    Create Application.php

---
+++ includes/Core/Application.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dizzy\Events\Core;
+
+use Dizzy\Events\Providers\EventServiceProvider;
+
+defined('ABSPATH') || exit;
+
+/**
+ * Application bootstrap.
+ *
+ * Creates and manages application services.
+ *
+ * @package Dizzy\Events\Core
+ */
+final class Application
+{
+    /**
+     * Service container.
+     */
+    private Container $container;
+
+    /**
+     * Constructor.
+     */
+    public function __construct()
+    {
+        $this->container = new Container();
+    }
+
+    /**
+     * Boot application.
+     */
+    public function boot(): void
+    {
+        $this->registerProviders();
+    }
+
+    /**
+     * Get container instance.
+     */
+    public function container(): Container
+    {
+        return $this->container;
+    }
+
+    /**
+     * Register service providers.
+     */
+    private function registerProviders(): void
+    {
+        $providers = [
+            EventServiceProvider::class,
+        ];
+
+        foreach ($providers as $provider) {
+
+            (new $provider())
+                ->register($this->container);
+        }
+    }
+}
