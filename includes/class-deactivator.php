b9224d51294cae17b7b495c9cda179a0c0b7871d
Poserinka <111547033+Poserinka@users.noreply.github.com>
2026-07-31 05:44:07 +0200

    Update class-deactivator.php
    Add plugin deactivator class v0.1-alpha.1

--- includes/class-deactivator.php
+++ includes/class-deactivator.php
@@ -1,1 +1,28 @@
+<?php
 
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+
+class Dizzy_Deactivator {
+
+
+    /**
+     * Plugin deactivation
+     */
+    public static function deactivate() {
+
+
+        /**
+         * Clear rewrite rules
+         *
+         * This keeps WordPress URLs clean
+         */
+        flush_rewrite_rules();
+
+
+    }
+
+
+}
