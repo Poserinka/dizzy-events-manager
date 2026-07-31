faed9764d90907c6704ca36ffcbb974d963f1e3a
Poserinka <111547033+Poserinka@users.noreply.github.com>
2026-07-31 05:44:37 +0200

    Update class-plugin.php
    Improve plugin bootstrap class v0.1-alpha.1

--- includes/class-plugin.php
+++ includes/class-plugin.php
@@ -1,1 +1,73 @@
+<?php
 
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+
+class Dizzy_Plugin {
+
+
+    /**
+     * Start plugin
+     */
+    public static function run() {
+
+
+        /**
+         * Load admin functionality
+         */
+        if ( is_admin() ) {
+
+            new Dizzy_Admin_Menu();
+
+        }
+
+
+        /**
+         * Register post types
+         */
+        new Dizzy_Events_Post_Type();
+
+        new Dizzy_Artists_Post_Type();
+
+
+        /**
+         * Load text domain
+         */
+        add_action(
+            'plugins_loaded',
+            array(
+                __CLASS__,
+                'load_textdomain'
+            )
+        );
+
+
+    }
+
+
+
+    /**
+     * Load translations
+     */
+    public static function load_textdomain() {
+
+
+        load_plugin_textdomain(
+
+            DIZZY_EVENTS_TEXTDOMAIN,
+
+            false,
+
+            dirname(
+                DIZZY_EVENTS_BASENAME
+            ) . '/languages/'
+
+        );
+
+
+    }
+
+
+}
