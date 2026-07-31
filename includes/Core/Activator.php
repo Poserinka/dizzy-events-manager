424f7d8ca20f1c2d92e832915eb34020b521f78c
Poserinka <info@poserinka.com>
2026-07-31 23:12:29 +0200

    Core

--- includes/Core/Activator.php
+++ includes/Core/Activator.php
@@ -1,10 +1,44 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Untitled Document</title>
-</head>
+<?php
+/**
+ * Plugin Activator.
+ *
+ * Runs once when the plugin is activated.
+ *
+ * @package DizzyEventsManager
+ */
 
-<body>
-</body>
-</html>
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+class Dizzy_Activator {
+
+	/**
+	 * Activate plugin.
+	 *
+	 * @return void
+	 */
+	public static function activate() {
+
+		// Install database and plugin defaults.
+		if ( class_exists( 'Dizzy_Installer' ) ) {
+			Dizzy_Installer::install();
+		}
+
+		// Store current plugin version.
+		update_option(
+			'dizzy_events_version',
+			DIZZY_EVENTS_VERSION
+		);
+
+		// Store current database version.
+		update_option(
+			'dizzy_events_db_version',
+			DIZZY_EVENTS_DB_VERSION
+		);
+
+		// Refresh rewrite rules.
+		flush_rewrite_rules();
+
+	}
+}
--- includes/Core/Plugin.php
+++ includes/Core/Plugin.php
@@ -1,10 +1,76 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Untitled Document</title>
-</head>
+<?php
+/**
+ * Core Plugin Class
+ *
+ * @package DizzyEventsManager
+ */
 
-<body>
-</body>
-</html>
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+class Dizzy_Plugin {
+
+	/**
+	 * Boot plugin.
+	 *
+	 * @return void
+	 */
+	public static function run() {
+
+		self::load_textdomain();
+		self::boot_modules();
+
+	}
+
+	/**
+	 * Load translations.
+	 *
+	 * @return void
+	 */
+	private static function load_textdomain() {
+
+		load_plugin_textdomain(
+			DIZZY_EVENTS_TEXTDOMAIN,
+			false,
+			dirname( DIZZY_EVENTS_BASENAME ) . '/languages'
+		);
+
+	}
+
+	/**
+	 * Initialize all plugin modules.
+	 *
+	 * @return void
+	 */
+	private static function boot_modules() {
+
+		$modules = array(
+
+			'Dizzy_Assets',
+			'Dizzy_Admin',
+			'Dizzy_Events',
+			'Dizzy_Reservations',
+			'Dizzy_Checkin',
+			'Dizzy_Reports',
+			'Dizzy_Posters',
+			'Dizzy_Social',
+
+		);
+
+		foreach ( $modules as $module ) {
+
+			if (
+				class_exists( $module ) &&
+				method_exists( $module, 'init' )
+			) {
+
+				$module::init();
+
+			}
+
+		}
+
+	}
+
+}
