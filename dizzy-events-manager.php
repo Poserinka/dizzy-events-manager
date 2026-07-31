3a18cf801d2d5dc0893c54ffd0addab1cbe00e42
Poserinka <info@poserinka.com>
2026-07-31 23:36:14 +0200

    Update dizzy-events-manager.php

--- dizzy-events-manager.php
+++ dizzy-events-manager.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Dizzy Events Manager
  * Plugin URI:        https://github.com/Poserinka/dizzy-events-manager
  * Description:       Professional event management system for Jazzcafé Dizzy.
- * Version:           1.0.0-dev
+ * Version:           1.0.0
  * Requires at least: 6.5
  * Requires PHP:      8.1
  * Author:            Poserinka Design
@@ -16,39 +16,42 @@
  * @package DizzyEventsManager
  */
 
+declare(strict_types=1);
+
 defined( 'ABSPATH' ) || exit;
 
-/*
-|--------------------------------------------------------------------------
-| Plugin Constants
-|--------------------------------------------------------------------------
-*/
-
-define( 'DIZZY_EVENTS_VERSION', '1.0.0-dev' );
+/**
+ * Plugin constants.
+ */
+define( 'DIZZY_EVENTS_VERSION', '1.0.0' );
 define( 'DIZZY_EVENTS_DB_VERSION', '1.0.0' );
 
 define( 'DIZZY_EVENTS_FILE', __FILE__ );
-define( 'DIZZY_EVENTS_BASENAME', plugin_basename( DIZZY_EVENTS_FILE ) );
 define( 'DIZZY_EVENTS_PATH', plugin_dir_path( DIZZY_EVENTS_FILE ) );
 define( 'DIZZY_EVENTS_URL', plugin_dir_url( DIZZY_EVENTS_FILE ) );
+define( 'DIZZY_EVENTS_BASENAME', plugin_basename( DIZZY_EVENTS_FILE ) );
 define( 'DIZZY_EVENTS_TEXTDOMAIN', 'dizzy-events-manager' );
 
-/*
-|--------------------------------------------------------------------------
-| Load Autoloader
-|--------------------------------------------------------------------------
-*/
+/**
+ * Load Composer autoloader if available.
+ */
+if ( file_exists( DIZZY_EVENTS_PATH . 'vendor/autoload.php' ) ) {
+	require_once DIZZY_EVENTS_PATH . 'vendor/autoload.php';
+}
 
+/**
+ * Load plugin autoloader.
+ */
 require_once DIZZY_EVENTS_PATH . 'includes/Core/Autoloader.php';
 
+/**
+ * Register autoloader.
+ */
 DizzyEvents\Core\Autoloader::register();
 
-/*
-|--------------------------------------------------------------------------
-| Activation / Deactivation
-|--------------------------------------------------------------------------
-*/
-
+/**
+ * Activation hook.
+ */
 register_activation_hook(
 	DIZZY_EVENTS_FILE,
 	array(
@@ -57,6 +60,9 @@ register_activation_hook(
 	)
 );
 
+/**
+ * Deactivation hook.
+ */
 register_deactivation_hook(
 	DIZZY_EVENTS_FILE,
 	array(
@@ -65,10 +71,7 @@ register_deactivation_hook(
 	)
 );
 
-/*
-|--------------------------------------------------------------------------
-| Bootstrap Plugin
-|--------------------------------------------------------------------------
-*/
-
+/**
+ * Bootstrap plugin.
+ */
 DizzyEvents\Core\Plugin::init();
