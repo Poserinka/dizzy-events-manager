66b005b5963bfb797af196d5b6044dd70e41f441
Poserinka <111547033+Poserinka@users.noreply.github.com>
2026-07-31 05:45:20 +0200

    Update helpers.php

--- includes/helpers.php
+++ includes/helpers.php
@@ -1,1 +1,70 @@
 
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+
+/**
+ * Get event default venue
+ *
+ * @return string
+ */
+function dizzy_get_default_venue() {
+
+    return 'Jazzcafé Dizzy';
+
+}
+
+
+/**
+ * Get event default address
+ *
+ * @return string
+ */
+function dizzy_get_default_address() {
+
+    return "'s-Gravendijkwal 127, 3021 EK Rotterdam";
+
+}
+
+
+/**
+ * Get event default Google Maps URL
+ *
+ * @return string
+ */
+function dizzy_get_default_maps_url() {
+
+    return 'https://maps.app.goo.gl/t73PkgDRtb6RvKFMA';
+
+}
+
+
+/**
+ * Format ticket price
+ *
+ * @param float $price
+ *
+ * @return string
+ */
+function dizzy_format_price( $price ) {
+
+
+    if ( empty( $price ) ) {
+
+        return '';
+
+    }
+
+
+    return '€' . number_format(
+        (float) $price,
+        2,
+        ',',
+        '.'
+    );
+
+
+}
