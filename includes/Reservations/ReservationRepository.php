204c715ca3481692d8362647a9d6e60d339edbb4
Poserinka <111547033+Poserinka@users.noreply.github.com>
2026-08-02 00:06:29 +0200

    Add ReservationRepository for new reservations architecture

---
+++ includes/Reservations/ReservationRepository.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dizzy\Events\Reservations;
+
+defined('ABSPATH') || exit;
+
+final class ReservationRepository
+{
+    public function find(int $reservationId): ?array
+    {
+        return null;
+    }
+
+    public function save(array $data): int
+    {
+        return 0;
+    }
+}
