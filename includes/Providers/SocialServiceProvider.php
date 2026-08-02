a847e5aaa2d332bce243463db4060dd741d3a25b
Poserinka <111547033+Poserinka@users.noreply.github.com>
2026-08-02 02:31:02 +0200

    Update SocialServiceProvider.php

--- includes/Providers/SocialServiceProvider.php
+++ includes/Providers/SocialServiceProvider.php
@@ -1,1 +1,33 @@
+<?php
 
+declare(strict_types=1);
+
+namespace Dizzy\Events\Social\Providers;
+
+use Dizzy\Events\Core\Container;
+use Dizzy\Events\Social\Repositories\SocialRepository;
+use Dizzy\Events\Social\Services\SocialService;
+
+defined('ABSPATH') || exit;
+
+final class SocialServiceProvider
+{
+    public function register(Container $container): void
+    {
+        $container->singleton(
+            SocialRepository::class,
+            static function (): SocialRepository {
+                return new SocialRepository();
+            }
+        );
+
+        $container->singleton(
+            SocialService::class,
+            static function (Container $container): SocialService {
+                return new SocialService(
+                    $container->get(SocialRepository::class)
+                );
+            }
+        );
+    }
+}
