7cbd365665f06a70ae3423c6426382a035d573bc
Poserinka <111547033+Poserinka@users.noreply.github.com>
2026-08-02 13:20:31 +0200

    Add Reports repository domain layer

---
+++ includes/Reports/Repositories/ReportRepository.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dizzy\Events\Reports\Repositories;
+
+defined('ABSPATH') || exit;
+
+final class ReportRepository
+{
+    public function getReservations(): array
+    {
+        return [];
+    }
+
+    public function getAttendance(): array
+    {
+        return [];
+    }
+}
