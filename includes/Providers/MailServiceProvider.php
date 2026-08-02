8f897cf0344c67fd05156b99bf6e8bf582957c38
Poserinka <111547033+Poserinka@users.noreply.github.com>
2026-08-02 13:32:03 +0200

    Refactor MailServiceProvider to new Mail domain

--- includes/Providers/MailServiceProvider.php
+++ includes/Providers/MailServiceProvider.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Dizzy\Events\Providers;
 
 use Dizzy\Events\Core\Container;
-use Dizzy\Events\Mail\Mailer;
+use Dizzy\Events\Mail\Services\MailService;
 
 defined('ABSPATH') || exit;
 
@@ -14,9 +14,9 @@ final class MailServiceProvider
     public function register(Container $container): void
     {
         $container->singleton(
-            Mailer::class,
-            static function (): Mailer {
-                return new Mailer();
+            MailService::class,
+            static function (): MailService {
+                return new MailService();
             }
         );
     }
