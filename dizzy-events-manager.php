3af720d9682cd16c8af545b6a2ab305962d719b1
Poserinka <111547033+Poserinka@users.noreply.github.com>
2026-08-03 02:51:45 +0200

    Bump events manager to 2.0.3

--- dizzy-events-manager.php
+++ dizzy-events-manager.php
@@ -4,7 +4,7 @@
  * Plugin Name: Dizzy Events Manager
  * Plugin URI: https://github.com/Poserinka/dizzy-events-manager
  * Description: Advanced event management system for Dizzy Rotterdam.
- * Version: 2.0.2
+ * Version: 2.0.3
  * Author: Poserinka Design
  * Author URI: https://poserinka.com
  * Text Domain: dizzy-events-manager
@@ -17,7 +17,7 @@ defined('ABSPATH') || exit;
 
 define(
     'DIZZY_EVENTS_VERSION',
-    '2.0.2'
+    '2.0.3'
 );
 
 define(
